1a1afa09279155eb5c6a075076cee2f3dfa196e6
dlotts@knightrider.com <dlotts@knightrider.com>
2021-12-02 13:34:04 -0600

    feat: wip

---
+++ src/app/services/organization-list.service.ts
@@ -0,0 +1,22 @@
+// /src/app/services/organization-list.service.ts
+
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
+
+import { Organization } from "../models/Organization";
+
+@Injectable({
+  providedIn: "root",
+})
+export class OrganizationListService {
+  constructor(private http: HttpClient) {}
+
+  getOrganizationList(): Observable<Organization[]> {
+    return this.http.get<Organization[]>("/.netlify/functions/getOrgs", {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  }
+}
